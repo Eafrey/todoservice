b42e79f4cfb8e202cef140b645290098281ff141
Eafrey <1019812436@qq.com>
2018-08-07 17:39:43 +0800

    1800

--- src/main/java/com/thoughtworks/traing/chensen/todoservice/security/ToDoAuthFilter.java
+++ src/main/java/com/thoughtworks/traing/chensen/todoservice/security/ToDoAuthFilter.java
@@ -3,18 +3,12 @@ package com.thoughtworks.traing.chensen.todoservice.security;
 import com.google.common.collect.ImmutableList;
 import com.thoughtworks.traing.chensen.todoservice.client.UserClient;
 import com.thoughtworks.traing.chensen.todoservice.dto.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.thymeleaf.util.StringUtils;
 
@@ -23,12 +17,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
-
-import static com.sun.scenario.Settings.set;
 
 @Component
 public class ToDoAuthFilter extends OncePerRequestFilter {
