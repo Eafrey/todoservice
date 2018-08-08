0600fc8dfc646e79149d93ed1dc97edb2b75bf5d
Eafrey <1019812436@qq.com>
2018-08-08 10:09:23 +0800

    correct---complete auth in user service

--- src/main/java/com/thoughtworks/traing/chensen/todoservice/security/ToDoAuthFilter.java
+++ src/main/java/com/thoughtworks/traing/chensen/todoservice/security/ToDoAuthFilter.java
@@ -1,8 +1,6 @@
 package com.thoughtworks.traing.chensen.todoservice.security;
 
 import com.google.common.collect.ImmutableList;
-import com.thoughtworks.traing.chensen.todoservice.client.UserClient;
-import com.thoughtworks.traing.chensen.todoservice.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,46 +19,21 @@ import java.io.IOException;
 @Component
 public class ToDoAuthFilter extends OncePerRequestFilter {
 
-//    private static final byte[] SECRET_KEY = "kitty".getBytes(Charset.defaultCharset());
-
-    @Autowired
-    private UserClient userClient;
-//
-//    public static String generateToken(int id) {
-//        Map<String, Object> claims = new HashMap<>();
-//        claims.put("id", id);
-//
-//        String token = Jwts.builder()
-//                .addClaims(claims)
-//                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
-//                .compact();
-//        return token;
-//    }
-
-
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
         String token = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (!StringUtils.isEmpty(token)) {
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.add("Authorization", token);
-
-//            RestTemplate restTemplate = new RestTemplate();
-//            String userResourceUrl = "http://localhost:8081/api/verification";
-
-//                ResponseEntity<User> userResponse = restTemplate.postForEntity(userResourceUrl, token, User.class);
-//                User userResponseBody = userResponse.getBody();
             try {
-                User user = userClient.verifyTokenInternal(token);
+                String[] tokens = token.split(":");
+                int id = Integer.parseInt(tokens[0]);
 
                 SecurityContextHolder.getContext().setAuthentication(
-                        new UsernamePasswordAuthenticationToken(user.getId(), null,
+                        new UsernamePasswordAuthenticationToken(id, null,
                                 ImmutableList.of(new SimpleGrantedAuthority("admin"),
                                         new SimpleGrantedAuthority("role")))
                 );
-//                }
             } catch (Exception e) {
             } finally {
                 filterChain.doFilter(request, response);
@@ -70,12 +43,6 @@ public class ToDoAuthFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
         }
 
-
-//        SecurityContextHolder.getContext().setAuthentication(
-//                new UsernamePasswordAuthenticationToken("user", null,
-//                        ImmutableList.of(new SimpleGrantedAuthority("admin"),
-//                                new SimpleGrantedAuthority("role")))
-//        );
 
     }
 
